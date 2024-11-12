5452a1f92128b5a5ab59ea20434d4f321b7fb11f
iarko26 <arnobghosh34@gmail.com>
2024-11-13 01:58:00 +0600

    First

---
+++ ChatApp/client/src/App.jsx
@@ -0,0 +1,25 @@
+
+import { Button } from "./components/ui/button"
+import { Navigate, Route,Routes } from "react-router-dom"
+import Auth from "./pages/auth/Auth"
+import Chat from "./pages/chat/Chat"
+import Profile from "./pages/profile/Profile"
+function App() {
+  
+
+  return (
+   <div >
+        <Routes>
+           <Route path="/auth" element={<Auth/>} />
+           <Route path="/chat" element={<Chat/>} />
+           <Route path="/profile" element={<Profile/>} />
+           <Route path="*" element={
+            <Navigate to={"/auth"} />
+           } />
+        </Routes>
+        
+   </div>
+  )
+}
+
+export default App
---
+++ ChatApp/client/src/pages/auth/Auth.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+
+function Auth() {
+  return (
+    <div className='h-[100vh] w-[100vw] flex items-center justify-center'>
+        <div className='w-[40vw] h-[80vh] bg-[#FFF0E5] border-1 shadow-2xl text-opacity-90 rounded-md md:w-[50vw] lg:[30vw] xl:w-[20vw] grid xl:grid-cols-2'>
+            <div className='flex flex-col gap-10 items-center justify-center'>
+                <div className=''>
+
+                </div>
+            </div>
+        </div>
+    </div> 
+  )
+}
+
+export default Auth
